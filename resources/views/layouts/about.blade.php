4129df9fb3adf03c43c9ce7704844eda933c5811
abbey <adetayoabiodun4@gmail.com>
2019-11-22 11:37:11 +0100

    about-change2

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -3,19 +3,37 @@
       <div class="text-center">
          <div class="section-title">
             <h4>Who are we</h4>
+            
          </div>
          <div class="container-fluid">
-            <div class="row">
+            <div class="row mt-5">
+                  <div class="col-md-6">
+                        <!-- Card -->
+                        <div class="card card-image" style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);">
+      
+                           <!-- Content -->
+                           <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
+                           <div>
+                              <h2 class="white-text" style="margin-bottom: 20px;"><i class="fas fa-chart-pie" style="color: green;"></i> Grotegy</h2>
+                              {{-- <h3 class="card-title pt-2"><strong>This is the card title</strong></h3> --}}
+                              <p style="margin-bottom: 30px;"> If you want to scale your business, you need a strategy. This strategy has to be structured and sustainable. Whether you’re looking to discover your brand or develop a viable growth plan underpinned by sound sales strategy, we have you covered. We help businesses discover the voice behind their brand... One idea at a time</p>
+                              
+                           </div>
+                           </div>
+                        
+                        </div>
+                        <!-- Card -->
+                     </div>
                <div class="col-md-6">
-                  <p class="text-center" style="color: #000;">
-                     If you want to scale your business, you need a strategy. This strategy has to be structured and sustainable. Whether you’re looking to discover your brand or develop a viable growth plan underpinned by sound sales strategy, we have you covered. We help businesses discover the voice behind their brand... One idea at a time
+                  <p class="text-center" style="margin-top: 15px; color: black;">
+                        Grotegy is a growth, strategy, legal, compliance and business consulting firm that is defining the way African startups and businesses plan and execute their growth strategies. So Whether you’re looking to discover your brand or develop a viable growth plan underpinned by sound sales strategy, we will help your African business build, grow and scale globally faster and more efficiently than you can on your own. 
+                        
+                        From efficiency to sustainability, our goal is to help you always to be clear, thoughtful, and focused on outcomes. In short, we want to make getting to results faster and easier for everyone.. one idea at a time.
                      
                   </p>
                <button type="button" class="btn btn-outline-success btn-rounded waves-effect" style="border-radius: 20px;"> <a href="{{ route('aboutPage') }}">Read more<i class="fas fa-long-arrow-alt-right"></i></a></button>
                </div>
-               <div class="col-md-6">
-                  <img src="" alt="">
-               </div>
+               
             </div>
                  
          </div>
