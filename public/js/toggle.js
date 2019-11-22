fd08f175af8213a549409765368591cf145e4600
abbey <adetayoabiodun4@gmail.com>
2019-11-22 13:58:54 +0100

    about-changess

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,30 +1,5 @@
-// const navSlide = () => {
-//   const burger = document.querySelector('.burger');
-//   const nav = document.querySelector('.nav-link');
-//   const navLinks = document.querySelectorAll('.nav-link li');
-
-//   // toggle nav
-//   burger.addEventListener('click', ()=>{
-//       nav.classList.toggle('nav-active');
-//       // animate nav
-//       navLinks.forEach((link, index)=>{
-//           if(link.style.animation){
-//               link.style.animation = '';
-//           }
-//           else{
-//               link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-//           }
-//       });
-//       // burger animate
-//       burger.classList.toggle('toggle');
-//   });
-
-  
-// }
-// navSlide(); 
-
-
-
+   
+   
    wow = new WOW(
       {
         animateClass: 'animated',
@@ -35,8 +10,4 @@
       }
     );
     wow.init();
-    // document.getElementById('moar').onclick = function() {
-    //   var section = document.createElement('section');
-    //   section.className = 'section--purple wow fadeInDown';
-    //   this.parentNode.insertBefore(section, this);
-    // };
+    
