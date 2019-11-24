36db8a0a198eb30024263793ad88d03c6380b9d3
abbey <adetayoabiodun4@gmail.com>
2019-11-25 00:10:35 +0100

    nav-adjusted1

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,20 +4,20 @@
 @endsection
 <!--Main Navigation-->
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark " style="background-color: #fff; border-bottom: 1px solid green;">
-    <a class="navbar-brand" href="#" style="color: #000;"><strong>Navbar</strong></a>
+    <a class="navbar-brand" href="#" style="color: #000;"><strong>Grotegy</strong></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background-color: green; color: blue;">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto" style="margin-right: 30px;">
             <li class="nav-item active">
-                <a class="nav-link" href="#" style="color: #000;">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#" style="color: #000;">Portfolio <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" style="color: #000;">Link</a>
+                <a class="nav-link" href="#" style="color: #000;">Contact</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" style="color: #000;">Profile</a>
+                <a class="nav-link" href="#" style="color: #000;">Blog</a>
             </li>
         </ul>
     </div>
