a56ec7de14efc7943c1258a3e02f414fd15610a0
abbey <adetayoabiodun4@gmail.com>
2019-12-02 12:35:23 +0100

    acc

--- resources/views/layouts/services.blade.php
+++ resources/views/layouts/services.blade.php
@@ -1,55 +1,52 @@
-<section class="services pt-100 pb-50" id="services">
-    <div class="container">
-       <div class="row">
-          <div class="col-xl-6 mx-auto text-center">
-             <div class="section-title mb-100">
-                <h4>my services</h4>
-             </div>
-          </div>
-       </div>
-       <div class="row"> 
-          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-             <!-- Single Service -->
-             <div class="single-service">
-                <i class="fa fa-laptop"></i>
-                <h4>Brand discovery</h4>
-                <p>Understand your market, get organized, grow sales. For insight through execution, We help you get ahead of change and close the gap between analysis and planning</p>
-                  {{-- <div class="row">
-                     <div class="col-md-6"><p>Idea Assessment</p></div>
-                     <div class="col-md-6"><p>Value Proposition</p></div>
-                     
-                  </div>
-                  <div class="row">
-                     <div class="col-md-3"><p>Naming</p></div>
-                     <div class="col-md-9"><p>Brand action worksheet</p></div>
-                  </div> --}}
+<section id="service" style="background: green; margin-top: 50px; margin-bottom: 50px;">
+   <div class="container">
+   <div class="row">
+       <div class="col-md-12">
+           <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true"style="margin-top: 50px; margin-bottom: 50px;">
+               <div class="panel panel-default">
+                   <div class="panel-heading" role="tab" id="headingOne">
+                       <h4 class="panel-title">
+                           <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                               Section 1
+                           </a>
+                       </h4>
+                   </div>
+                   <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                       <div class="panel-body">
+                           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nisl lorem, dictum id pellentesque at, vestibulum ut arcu. Curabitur erat libero, egestas eu tincidunt ac, rutrum ac justo. Vivamus condimentum laoreet lectus, blandit posuere tortor aliquam vitae. Curabitur molestie eros. </p>
+                       </div>
+                   </div>
                </div>
-          </div>
-          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-             <!-- Single Service -->
-             <div class="single-service">
-                <i class="fa fa-gears"></i>  
-                <h4>Growth Hacking / Strategy</h4>
-                <p><blockquote>Setting business goals without plan is a wish</blockquote> Standing is noot an option. We make it easy to engage your target customers to generate revenue and achieve your goals</p>
-             </div>
-          </div>
-          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-             <!-- Single Service -->
-             <div class="single-service">
-                <i class="fa fa-mobile"></i>
-                <h4>Partnership</h4>
-                <p>We have the roadmap, required partnerships to navigate your survival and plot your growth trajectory. </p>
-             </div>
-          </div>
-          <div class="col-lg-6 col-md-6">
-             <!-- Single Service -->
-             <div class="single-service">
-                <i class="fa fa-magic"></i>
-                <h4>GroLegal</h4>
-                <p>From documentation, to legal representation, we have the required expertise in holding your hands through every step.</p>
-             </div>
-          </div>
-          
+               <div class="panel panel-default">
+                   <div class="panel-heading" role="tab" id="headingTwo">
+                       <h4 class="panel-title">
+                           <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                               Section 2
+                           </a>
+                       </h4>
+                   </div>
+                   <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                       <div class="panel-body">
+                           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nisl lorem, dictum id pellentesque at, vestibulum ut arcu. Curabitur erat libero, egestas eu tincidunt ac, rutrum ac justo. Vivamus condimentum laoreet lectus, blandit posuere tortor aliquam vitae. Curabitur molestie eros. </p>
+                       </div>
+                   </div>
+               </div>
+               <div class="panel panel-default">
+                   <div class="panel-heading" role="tab" id="headingThree">
+                       <h4 class="panel-title">
+                           <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                               Section 3
+                           </a>
+                       </h4>
+                   </div>
+                   <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                       <div class="panel-body">
+                           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nisl lorem, dictum id pellentesque at, vestibulum ut arcu. Curabitur erat libero, egestas eu tincidunt ac, rutrum ac justo. Vivamus condimentum laoreet lectus, blandit posuere tortor aliquam vitae. Curabitur molestie eros. </p>
+                       </div>
+                   </div>
+               </div>
+           </div>
        </div>
-    </div>
- </section>
+   </div>
+</div>
+</section>
