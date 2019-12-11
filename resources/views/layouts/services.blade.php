56d51de7db7031483e50ea3bc6e748fce6bcd45e
abbey <adetayoabiodun4@gmail.com>
2019-12-11 14:13:02 +0100

    accordion fix modified

--- resources/views/layouts/services.blade.php
+++ resources/views/layouts/services.blade.php
@@ -1,3 +1,6 @@
+<section>
+  <h3 class="text-left">Services</h3>
+</section>
 <div id="accordion">
     <div class="card">
       <div class="card-header" id="headingOne">
@@ -13,7 +16,7 @@
           <p>‘’Understand your market, get organized, grow sales. From insight through execution,
               we help you get ahead of change and close the gap between analysis and planning
               </p>
-              <marquee style="margin-top: 10px;" behavior="alternate" direction="left">
+              <marquee style="margin-top: 10px;" width = "100%" direction="left" >
                 <ul class="text-center">
                   <li><i class="far fa-lightbulb mr-2"></i>Idea assessment</li>
                   <li><i class="fas fa-coins mr-2"></i>Value Proposition</li>
@@ -21,6 +24,10 @@
                   <li><i class="fas fa-file-signature mr-2"></i>Naming</li>
                 </ul>
             </marquee>
+            <section class="text-center">
+                <button type="button" class="btn btn-outline-success btn-rounded waves-effect text-center" style="border-radius: 20px;" > Let's Talk</button>
+            </section>
+            
         </div>
       </div>
     </div>
@@ -32,7 +39,6 @@
           </button>
 
         </h5>
-        <h4>yoo</h4>
       </div>
       <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
         <div class="card-body">
@@ -40,7 +46,7 @@
               Standing still is not an option. We make it easy for you to engage your target customers to generate
               revenue and achieve your goals.
           </p>
-          <marquee style="margin-top: 10px;" width = "50%" behavior="alternate" direction="left">
+          <marquee style="margin-top: 10px;" width = "100%" direction="left">
               <ul class="text-center">
                 <li><i class="far fa-lightbulb mr-2"></i>Customer acquisition</li>
                 <li><i class="fas fa-coins mr-2"></i>Business goals</li>
@@ -50,54 +56,64 @@
                 <li><i class="fas fa-file-signature mr-2"></i>New channels</li>
               </ul>
           </marquee>
+          <section class="text-center">
+              <button type="button" class="btn btn-outline-success btn-rounded waves-effect text-center" style="border-radius: 20px;" > Let's Talk</button>
+          </section>
         </div>
       </div>
     </div>
     <div class="card">
-      <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-          <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-              Partnerships
-          </button>
-        </h5>
-      </div>
-      <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-          <p>We have the roadmap, required partnerships to navigate your survival and plot your
-              growth trajectory.
-          </p>
-          <marquee style="margin-top: 10px;">
-              <ul class="text-center">
-                <li><i class="far fa-lightbulb mr-2"></i>Identification</li>
-                <li><i class="fas fa-coins mr-2"></i>Market Evaluation</li>
-                <li><i class="fas fa-chess-pawn mr-2"></i>Partner evaluation</li>
-                <li><i class="fas fa-file-signature mr-2"></i>Closing the deal</li>
-                </ul>
-          </marquee>
+        <div class="card-header" id="headingThree">
+          <h5 class="mb-0">
+            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Partnerships
+            </button>
+          </h5>
         </div>
-      </div>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+            <div class="card-body">
+              <p>We have the roadmap, required partnerships to navigate your survival and plot your
+                  growth trajectory.
+              </p>
+              <marquee   width = "100%"  direction="left" style="margin-top: 10px;" >
+                  <ul class="text-center">
+                    <li><i class="far fa-lightbulb mr-2"></i>Identification</li>
+                    <li><i class="fas fa-coins mr-2"></i>Market Evaluation</li>
+                    <li><i class="fas fa-chess-pawn mr-2"></i>Partner evaluation</li>
+                    <li><i class="fas fa-file-signature mr-2"></i>Closing the deal</li>
+                    </ul>
+              </marquee>
+              <section class="text-center">
+                  <button type="button" class="btn btn-outline-success btn-rounded waves-effect text-center" style="border-radius: 20px;" > Let's Talk</button>
+              </section>
+            </div>
+          </div>
     </div>
     <div class="card">
         <div class="card-header" id="headingFour">
           <h5 class="mb-0">
             <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                Partnerships
+                GroLegal
             </button>
           </h5>
         </div>
+    </div>
         <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
           <div class="card-body">
-            <p>We have the roadmap, required partnerships to navigate your survival and plot your
-                growth trajectory.
+            <p>From documentation, to legal representation, we have the required expertise in holding your
+                hands through every step.
             </p>
-            <marquee  behavior="alternate" direction="left" style="margin-top: 10px;">
+            <marquee   width = "100%" direction="left" style="margin-top: 10px;">
                 <ul class="text-center">
-                  <li><i class="far fa-lightbulb mr-2"></i>Identification</li>
-                  <li><i class="fas fa-coins mr-2"></i>Market Evaluation</li>
-                  <li><i class="fas fa-chess-pawn mr-2"></i>Partner evaluation</li>
-                  <li><i class="fas fa-file-signature mr-2"></i>Closing the deal</li>
+                  <li><i class="far fa-lightbulb mr-2"></i>Privacy Notice</li>
+                  <li><i class="fas fa-coins mr-2"></i>Terms of service</li>
+                  <li><i class="fas fa-chess-pawn mr-2"></i>Contract</li>
+                  <li><i class="fas fa-file-signature mr-2"></i>IP related issues</li>
                   </ul>
             </marquee>
+            <section class="text-center">
+                <button type="button" class="btn btn-outline-success btn-rounded waves-effect text-center" style="border-radius: 20px;" > Let's Talk</button>
+            </section>
           </div>
         </div>
       </div>
